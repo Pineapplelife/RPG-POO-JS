c19900b473ea956dd73f94209ac50d2da983bffe
Unknown <pierre.faye@edu.itescia.fr>
2018-12-12 16:50:41 +0100

    Level up

--- poojs_test.js
+++ poojs_test.js
@@ -12,87 +12,160 @@ function Perso (firstName, lastName, classNumber, health, healthMax, damage, exp
 	this.fullName = function() {
     	return this.firstName + " " + this.lastName;
 	},
-	/*class : function() {
-    	if (this.Classe = 1){
-    		this.HealthMax = (this.HealthMax/100)*130
-		}
-		else if (this.Classe = 2){
-			this.HealthMax = (this.HealthMax/100)*75
-			this.Damage = (this.Damage/100)*130
-		}
-		else{
-			this.Damage = (this.Damage/100)*120
-		}
-	}*/
+	
 	this.levelUp = function() {
 		if (this.exp >= 100){
 			this.level ++;
 			this.exp = this.exp-100;
+			return this.level
 		}
 
 	},
 	this.coup = function(enemie){
 		enemie.health -= this.damage;
+		return enemie.firstName + " " + enemie.health
+	}
+	this.soin = function(){
+		this.health = this.healthMax
+		return this.health
+	}
+	this.victory = function(){
+		this.exp += Math.floor(Math.random(1) * Math.floor(10))
+		return "tu as" + this.exp;
 	}
 
 }
 
 function Guerrier () {
-	this.HealthMax = (this.HealthMax/100)*130;
-	this.class = 1;
+	this.levelUp = function(){
+		if (this.exp >= 100){
+			this.level ++;
+			this.exp -= 100;
+			this.healthMax += 55;
+			this.damage += 5;
+			this.healthMax = parseInt(this.healthMax);
+			this.damage = parseInt(this.damage);
+			this.classNumber = 1;
+			return this.level;
+		}
+		else{
+			return "tu as" + this.exp;
+		}
+	}
 }
 Guerrier.prototype = new Perso;
 
 function Mage () {
-	this.HealthMax = (this.HealthMax/100)*75;
-	this.Damage = (this.Damage/15)*45;
-	this.class = 2;
+	this.levelUp = function(){
+		if (this.exp >= 100){
+			this.level ++;
+			this.exp -= 100;
+			this.healthMax += 25;
+			this.damage += 20;
+			this.healthMax = parseInt(this.healthMax);
+			this.damage = parseInt(this.damage);
+			this.classNumber = 1;
+			return this.level;
+		}
+		else{
+			return "tu as" + this.exp;
+		}
+	}
 }
 Mage.prototype = new Perso;
 
 function Archer () {
-	this.Damage = (this.Damage/15)*25;
-	this.class = 3;
+	this.levelUp = function(){
+		if (this.exp >= 100){
+			this.level ++;
+			this.exp -= 100;
+			this.healthMax += 30;
+			this.damage += 15;
+			this.healthMax = parseInt(this.healthMax);
+			this.damage = parseInt(this.damage);
+			this.classNumber = 1;
+			return this.level;
+		}
+		else{
+			return "tu as" + this.exp;
+		}
+	}
 }
 Archer.prototype = new Perso;
 
-var Bill = new Mage;
-	Bill.firstName = "Bill";
-	Bill.lastName = "Doe";
-var Tom  = new Archer;
-	Tom.firstName = "Tom";
-	Tom.lastName = "Doe";
-var John = new Guerrier; 
-	John.firstName = "John";
-	John.lastName = "Doe";
-
-
-var monPerso = new Perso("Bob", "Epongjjjje", 0, 100, 100, 15, 8, 7);
-
-console.log(monPerso.fullName());
-console.log(Bill.fullName());
-/*
-var perso2 = {
-	firstName: "Bill",
-	lastName : "Doe",
-	Classe : 0, // 1 = Guerrier && 2 = Mage && 3 = Archer
-	Health : 100, // Barre de vie de base qui est égale à 100 Points de vie
-	Damage : 15, // Dégât de base d'une valeur de 15
-	Exp : 0, // Expérience acquises allant de 0 à 100
-	Level : 1, // Niveau supérieur une fois que l'expérience est à 100 && Niveau de base = 1
-	fullName : function() {
-    	return this.firstName + " " + this.lastName;
-	}
-		class : function() {
-    	if (this.Classe = 1){
-    		this.HealthMax = (this.HealthMax/100)*130
-		}
-		else if (this.Classe = 2){
-			this.HealthMax = (this.HealthMax/100)*75
-			this.Damage = (this.Damage/100)*130
+function Berserker () {
+	this.levelUp = function(){
+		if (this.exp >= 100){
+			this.level ++;
+			this.exp -= 100;
+			this.healthMax += 36;
+			this.damage += 10;
+			this.healthMax = parseInt(this.healthMax);
+			this.damage = parseInt(this.damage);
+			this.classNumber = 1;
+			return this.level;
 		}
 		else{
-			this.Damage = (this.Damage/100)*120
+			return "tu as" + this.exp ;
 		}
+	}
 }
-*/
+Berserker.prototype = new Perso;
+
+var Bill = new Mage();
+	Bill.firstName = "Bill";
+	Bill.lastName = "Doe";
+	Bill.classNumber = 0;
+	Bill.health = 100;
+	Bill.healthMax = 100;
+	Bill.damage = 15;
+	Bill.exp = 0;
+	Bill.level = 1;
+
+var Tom  = new Archer();
+	Tom.firstName = "Tom";
+	Tom.lastName = "Doe";
+	Tom.classNumber = 0;
+	Tom.health = 100;
+	Tom.healthMax = 100;
+	Tom.damage = 15;
+	Tom.exp = 0;
+	Tom.level = 1;
+
+var John = new Guerrier();
+	John.firstName = "John";
+	John.lastName = "Doe";
+	John.classNumber = 0;
+	John.health = 100;
+	John.healthMax = 100;
+	John.damage = 15;
+	John.exp = 0;
+	John.level = 1;
+
+var Ruby = new Berserker();
+	Ruby.firstName = "Ruby";
+	Ruby.lastName = "rose";
+	Ruby.classNumber = 0;
+	Ruby.health = 100;
+	Ruby.healthMax = 100;
+	Ruby.damage = 15;
+	Ruby.exp = 0;
+	Ruby.level = 1;
+
+
+console.log(Tom.fullName());
+console.log(John.fullName());
+console.log(Ruby.fullName());
+
+Ruby.exp += 100;
+console.log(Ruby.levelUp());
+Ruby.exp += 20;
+console.log(Ruby.levelUp());
+Ruby.exp += 100;
+console.log(Ruby.levelUp());
+console.log(Ruby.soin());
+while (Ruby.health > 0 && Tom.health > 0) {
+  console.log(Ruby.coup(Tom))
+  console.log(Tom.coup(Ruby))
+}
+console.log(Ruby.victory())
