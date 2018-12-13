b9f563902cd64fdf5e0ac441e2bd3f522be3dd76
Unknown <pierre.faye@edu.itescia.fr>
2018-12-13 15:14:42 +0100

    Inventaire II

--- Inventaire.js
+++ Inventaire.js
@@ -9,61 +9,130 @@ function Inventaire (potionSoin, potionMana, potionForce, bonbonniv, barreExp, e
 
 	this.acha1 = function() {
     	if (or >= 10) {
-    		this.or -= 10
-    		this.potionSoin += 1
+    		this.or -= 10;
+    		this.potionSoin ++;
     		return this.potionSoin + " " + this.or;
     	}
     	else{
     		return this.or + " Degage le clodo";
     	}
     }
+    this.vie = function(utilisateur) {
+    	if (potionSoin > 0) {
+    		this.potionSoin --;
+    		utilisateur.health += 20;
+    		if (utilisateur.health > utilisateur.healthMax) {
+    			utilisateur.health = utilisateur.healthMax;
+    		}
+    	return utilisateur.health;
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
 	this.acha2 = function() {
     	if (or >= 15) {
-    		this.or -= 15
-    		this.potionMana += 1
+    		this.or -= 15;
+    		this.potionMana ++;
     		return this.potionMana + " " + this.or;
     	}
     	else{
     		return this.or + " Degage le clodo";
     	}
     }
+    this.magie = function(utilisateur) {
+    	if (potionMana > 0) {
+    		this.potionMoin --;
+    		utilisateur.mana += 10;
+    		if (utilisateur.mana > utilisateur.manaMax) {
+    			utilisateur.mana = utilisateur.manaMax;
+    		}
+    	return utilisateur.mana;
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
 	this.acha3 = function() {
     	if (or >= 25) {
-    		this.or -= 25
-    		this.potionForce += 1
+    		this.or -= 25;
+    		this.potionForce ++;
     		return this.potionForce + " " + this.or;
     	}
     	else{
     		return this.or + " Degage le clodo";
     	}
     }
+    this.force = function(utilisateur) {
+    	if (potionForce > 0) {
+    		this.potionForce --;
+    		utilisateur.damage += 2;
+    	return utilisateur.damage;
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
 	this.acha4 = function() {
     	if (or >= 100) {
-    		this.or -= 100
-    		this.bonbonniv += 1
+    		this.or -= 100;
+    		this.bonbonniv ++;
     		return this.bonbonniv+ " " + this.or;
     	}
     	else{
     		return this.or + " Degage le clodo";
     	}
     }
+    this.nivUp = function(utilisateur) {
+    	if (bonbonniv > 0) {
+    		this.bonbonniv --;
+    		utilisateur.Up();
+    	return utilisateur.level;
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
     this.acha5 = function() {
     	if (or >= 50) {
-    		this.or -= 50
-    		this.barreExp += 1
+    		this.or -= 50;
+    		this.barreExp ++;
     		return this.barreExp+ " " + this.or;
     	}
     	else{
     		return this.or + " Degage le clodo";
     	}
     }
+    this.expplus = function(utilisateur) {
+    	if (barreExp > 0) {
+    		this.barreExp --;
+    		utilisateur.exp = utilisateur.exp * 2;
+    		utilisateur.levelUp();
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
+
     this.acha6 = function() {
     	if (or >= 40) {
-    		this.or -= 40
-    		this.elexir += 1
+    		this.or -= 40;
+    		this.elexir ++;
     		return this.elexir+ " " + this.or;
     	}
     	else{
     		return this.or + " Degage le clodo";
+    	}
+    }
+    this.bien = function(utilisateur) {
+    	if (elexir > 0) {
+    		this.elexir--;
+    		utilisateur.health = utilisateur.healthMax;
+    		utilisateur.mana = utilisateur.manaMax;
+    	return utilisateur.health;
+    	return utilisateur.mana;
+    	}
+    	else{
+    		return "vous en avez pas";
     	}
     }
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function Perso (firstName, lastName, classNumber, health, healthMax, damage, exp, level) {
 	this.firstName = firstName,
 	this.lastName = lastName,
@@ -200,6 +199,148 @@ var Ruby = new Berserker();
 	Ruby.exp = 0;
 	Ruby.level = 1;
 
+
+
+function Inventaire (potionSoin, potionMana, potionForce, bonbonniv, barreExp, elexir, or) {
+	this.potionSoin = potionSoin,
+	this.potionMana = potionMana,
+	this.potionForce = potionForce,
+	this.bonbonniv = bonbonniv, 
+	this.barreExp = barreExp, 
+	this.elexir = elexir,
+	this.or = or,
+
+	this.acha1 = function() {
+    	if (or >= 10) {
+    		this.or -= 10;
+    		this.potionSoin ++;
+    		return this.potionSoin + " " + this.or;
+    	}
+    	else{
+    		return this.or + " Degage le clodo";
+    	}
+    }
+    this.vie = function(utilisateur) {
+    	if (potionSoin > 0) {
+    		this.potionSoin --;
+    		utilisateur.health += 20;
+    		if (utilisateur.health > utilisateur.healthMax) {
+    			utilisateur.health = utilisateur.healthMax;
+    		}
+    	return utilisateur.health;
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
+	this.acha2 = function() {
+    	if (or >= 15) {
+    		this.or -= 15;
+    		this.potionMana ++;
+    		return this.potionMana + " " + this.or;
+    	}
+    	else{
+    		return this.or + " Degage le clodo";
+    	}
+    }
+    this.magie = function(utilisateur) {
+    	if (potionMana > 0) {
+    		this.potionMoin --;
+    		utilisateur.mana += 10;
+    		if (utilisateur.mana > utilisateur.manaMax) {
+    			utilisateur.mana = utilisateur.manaMax;
+    		}
+    	return utilisateur.mana;
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
+	this.acha3 = function() {
+    	if (or >= 25) {
+    		this.or -= 25;
+    		this.potionForce ++;
+    		return this.potionForce + " " + this.or;
+    	}
+    	else{
+    		return this.or + " Degage le clodo";
+    	}
+    }
+    this.force = function(utilisateur) {
+    	if (potionForce > 0) {
+    		this.potionForce --;
+    		utilisateur.damage += 2;
+    	return utilisateur.damage;
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
+	this.acha4 = function() {
+    	if (or >= 100) {
+    		this.or -= 100;
+    		this.bonbonniv ++;
+    		return this.bonbonniv+ " " + this.or;
+    	}
+    	else{
+    		return this.or + " Degage le clodo";
+    	}
+    }
+    this.nivUp = function(utilisateur) {
+    	if (bonbonniv > 0) {
+    		this.bonbonniv --;
+    		utilisateur.Up();
+    	return utilisateur.level;
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
+    this.acha5 = function() {
+    	if (or >= 50) {
+    		this.or -= 50;
+    		this.barreExp ++;
+    		return this.barreExp+ " " + this.or;
+    	}
+    	else{
+    		return this.or + " Degage le clodo";
+    	}
+    }
+    this.expplus = function(utilisateur) {
+    	if (barreExp > 0) {
+    		this.barreExp --;
+    		utilisateur.exp = utilisateur.exp * 2;
+    		utilisateur.levelUp();
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
+
+    this.acha6 = function() {
+    	if (or >= 40) {
+    		this.or -= 40;
+    		this.elexir ++;
+    		return this.elexir+ " " + this.or;
+    	}
+    	else{
+    		return this.or + " Degage le clodo";
+    	}
+    }
+    this.bien = function(utilisateur) {
+    	if (elexir > 0) {
+    		this.elexir--;
+    		utilisateur.health = utilisateur.healthMax;
+    		utilisateur.mana = utilisateur.manaMax;
+    	return utilisateur.health;
+    	return utilisateur.mana;
+    	}
+    	else{
+    		return "vous en avez pas";
+    	}
+    }
+
+var Sac = new Inventaire(3, 2, 0, 0, 0, 1, 100);
 
 console.log(Tom.fullName());
 console.log(John.fullName());
